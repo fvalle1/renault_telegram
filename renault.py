d69d7c223932ce52b324a7b60264d5170ef02505
Filippo Valle <filippo_valle@protonmail.com>
2025-08-12 15:12:21 +0200

    format code

--- renault.py
+++ renault.py
@@ -21,46 +21,37 @@ def renault_login():
     global API_KEY, KAMEREON_API_KEY, BASE_URL, KEMERON_URL, LOGINID, PASSWORD
     session = requests.Session()
 
-    payload = {'ApiKey': API_KEY,
-               'loginID': LOGINID,
-               'password': PASSWORD}
-    files = [
-    ]
+    payload = {"ApiKey": API_KEY, "loginID": LOGINID, "password": PASSWORD}
+    files = []
     headers = {}
     response = session.request(
-        "POST",
-        BASE_URL +
-        "/accounts.login",
-        headers=headers,
-        data=payload,
-        files=files)
+        "POST", BASE_URL + "/accounts.login", headers=headers, data=payload, files=files
+    )
     sessionCookie = response.json()["sessionInfo"]["cookieValue"]
     print(f"Cookie: {sessionCookie}")
 
-    payload = {'login_token': sessionCookie,
-               'ApiKey': API_KEY,
-               "fields": "data.personId,data.gigyaDataCenter"}
+    payload = {
+        "login_token": sessionCookie,
+        "ApiKey": API_KEY,
+        "fields": "data.personId,data.gigyaDataCenter",
+    }
     files = []
     headers = {}
 
     response = session.request(
         "GET",
-        BASE_URL +
-        "/accounts.getAccountInfo",
+        BASE_URL + "/accounts.getAccountInfo",
         headers=headers,
         data=payload,
-        files=files)
+        files=files,
+    )
     person_id = response.json()["data"]["personId"]
 
     print(f"person ID: {person_id}")
 
     response = session.request(
-        "GET",
-        BASE_URL +
-        "/accounts.getJWT",
-        headers=headers,
-        data=payload,
-        files=files)
+        "GET", BASE_URL + "/accounts.getJWT", headers=headers, data=payload, files=files
+    )
     jwt = response.json()["id_token"]
 
     print(f"JWT: {jwt}")
@@ -68,17 +59,17 @@ def renault_login():
     headers = {}
     payload = {}
     headers = {
-        'Content-Type': 'application/vnd.api+json',
-        'apikey': KAMEREON_API_KEY,
-        'x-gigya-id_token': jwt,
+        "Content-Type": "application/vnd.api+json",
+        "apikey": KAMEREON_API_KEY,
+        "x-gigya-id_token": jwt,
     }
 
     response = session.request(
         "GET",
-        KEMERON_URL +
-        f"/persons/{person_id}?country=IT",
+        KEMERON_URL + f"/persons/{person_id}?country=IT",
         headers=headers,
-        data=payload)
+        data=payload,
+    )
 
     print(response.json()["accounts"][0])
     account_id = response.json()["accounts"][0]["accountId"]
@@ -90,15 +81,18 @@ def get_vin(session, headers, account_id):
     global KEMERON_URL
     response = session.request(
         "GET",
-        KEMERON_URL +
-        f"/accounts/{account_id}/vehicles?country=IT",
+        KEMERON_URL + f"/accounts/{account_id}/vehicles?country=IT",
         headers=headers,
-        data={})
-    vin = response.json()["vehicleLinks"][0]['vin']
+        data={},
+    )
+    vin = response.json()["vehicleLinks"][0]["vin"]
 
     for vehicle in response.json()["vehicleLinks"]:
-        print(vehicle["brand"], vehicle["vehicleDetails"]
-              ["model"]["label"], vehicle["vin"])
+        print(
+            vehicle["brand"],
+            vehicle["vehicleDetails"]["model"]["label"],
+            vehicle["vin"],
+        )
 
     return vin
 
@@ -110,10 +104,11 @@ def get_charging_status(session, headers, account_id, vin):
     global KEMERON_URL
     response = session.request(
         "GET",
-        KEMERON_URL +
-        f"/accounts/{account_id}/kamereon/kca/car-adapter/v2/cars/{vin}/battery-status?country=IT",
+        KEMERON_URL
+        + f"/accounts/{account_id}/kamereon/kca/car-adapter/v2/cars/{vin}/battery-status?country=IT",
         headers=headers,
-        data={})
+        data={},
+    )
     print(response.json())
     return response.json()
 
@@ -122,10 +117,11 @@ def get_cockpit(session, headers, account_id, vin):
     global KEMERON_URL
     response = session.request(
         "GET",
-        KEMERON_URL +
-        f"/accounts/{account_id}/kamereon/kca/car-adapter/v1/cars/{vin}/cockpit?country=IT",
+        KEMERON_URL
+        + f"/accounts/{account_id}/kamereon/kca/car-adapter/v1/cars/{vin}/cockpit?country=IT",
         headers=headers,
-        data={})
+        data={},
+    )
     print(response.json())
     return response.json()
 
@@ -133,17 +129,20 @@ def get_cockpit(session, headers, account_id, vin):
 def get_location(session, headers, account_id, vin):
     response = session.request(
         "GET",
-        KEMERON_URL +
-        f"/accounts/{account_id}/kamereon/kca/car-adapter/v1/cars/{vin}/location?country=IT",
+        KEMERON_URL
+        + f"/accounts/{account_id}/kamereon/kca/car-adapter/v1/cars/{vin}/location?country=IT",
         headers=headers,
-        data={})
+        data={},
+    )
     print(response.json())
     return response.json()
 
 
 def send_message(msg, parse_mode=""):
     global TELEGRAM_KEY, CHAT_ID
-    with requests.get(f"https://api.telegram.org/bot{TELEGRAM_KEY}/sendMessage?chat_id={CHAT_ID}&text={msg}&parse_mode={parse_mode}") as req:
+    with requests.get(
+        f"https://api.telegram.org/bot{TELEGRAM_KEY}/sendMessage?chat_id={CHAT_ID}&text={msg}&parse_mode={parse_mode}"
+    ) as req:
         print(req.text)
 
 
@@ -160,8 +159,7 @@ def run():
     print("session started")
     vin = get_vin(session, headers, account_id)
     while True:
-        print(count, 5 * 60 * 1. /
-              1)
+        print(count, 5 * 60 * 1.0 / 1)
         print(last_charge_status, charging_status)
         with requests.get(PING_URL) as req:
             if req.status_code != 200:
@@ -169,7 +167,9 @@ def run():
                 time.sleep(60)
                 continue
             print("Ping OK")
-        with requests.get(f"https://api.telegram.org/bot{TELEGRAM_KEY}/getUpdates?offset={offset}") as req:
+        with requests.get(
+            f"https://api.telegram.org/bot{TELEGRAM_KEY}/getUpdates?offset={offset}"
+        ) as req:
             print(req.text)
             if req.status_code != 200:
                 print(f"Error {req.text}")
@@ -178,28 +178,30 @@ def run():
             try:
                 response = req.json()
                 has_new_messages = len(response["result"]) > 0
-                if (count > 5 * 60 * 1. /
-                      1) or (has_new_messages):  # every 5 minutes
+                if (count > 5 * 60 * 1.0 / 1) or (has_new_messages):  # every 5 minutes
                     try:
                         car_state = get_charging_status(
-                            session, headers, account_id, vin)
-                        car_cockpit = get_cockpit(
-                            session, headers, account_id, vin)
+                            session, headers, account_id, vin
+                        )
+                        car_cockpit = get_cockpit(session, headers, account_id, vin)
                         battery_status = car_state["data"]["attributes"]["batteryLevel"]
                         charging_status = int(
                             round(
-                                10 *
-                                float(
-                                    car_state["data"]["attributes"]["chargingStatus"])))
+                                10
+                                * float(
+                                    car_state["data"]["attributes"]["chargingStatus"]
+                                )
+                            )
+                        )
                         plug_status = car_state["data"]["attributes"]["plugStatus"]
                         count = 0
                     except Exception as e:
                         print(e)
                         session, person_id, account_id, jwt, headers = renault_login()
                         headers = {
-                            'Content-Type': 'application/vnd.api+json',
-                            'apikey': KAMEREON_API_KEY,
-                            'x-gigya-id_token': jwt,
+                            "Content-Type": "application/vnd.api+json",
+                            "apikey": KAMEREON_API_KEY,
+                            "x-gigya-id_token": jwt,
                         }
                         vin = get_vin(session, headers, account_id)
                         continue
@@ -218,45 +220,48 @@ def run():
                     print(text)
                     if "/charge" in text:
                         send_message(f"Charge: {battery_status}%")
-                        send_message(("Not " if plug_status ==
-                                      0 else " ") + "Plugged in")
+                        send_message(
+                            ("Not " if plug_status == 0 else " ") + "Plugged in"
+                        )
                     if "/info" in text:
                         totalMileage = car_cockpit["data"]["attributes"]["totalMileage"]
                         fuelAutonomy = car_cockpit["data"]["attributes"]["fuelAutonomy"]
-                        batteryAutonomy = car_state["data"]["attributes"]["batteryAutonomy"]
+                        batteryAutonomy = car_state["data"]["attributes"][
+                            "batteryAutonomy"
+                        ]
                         send_message(f"Total Km: {totalMileage}Km")
-                        send_message(
-                            f"Autonomy: [{fuelAutonomy}+{batteryAutonomy}]Km")
+                        send_message(f"Autonomy: [{fuelAutonomy}+{batteryAutonomy}]Km")
                     if "/vin" in text:
                         send_message(f"VIN: {vin}")
                     if "/plate" in text:
                         send_message(f"Plate: {PLATE}")
                     if "/location" in text:
-                        location = get_location(
-                            session, headers, account_id, vin)
+                        location = get_location(session, headers, account_id, vin)
                         lon = location["data"]["attributes"]["gpsLongitude"]
                         lat = location["data"]["attributes"]["gpsLatitude"]
                         send_message(
                             f"[Location](https://www.openstreetmap.org/%3Fzoom=19%26mlat={lat}%26mlon={lon})",
-                            parse_mode="MarkdownV2")
+                            parse_mode="MarkdownV2",
+                        )
                         OLC = olc_encode(lat, lon, 10)
                         send_message(
                             f"<b>{OLC[:SEPARATOR_POSITION_]}%2B{OLC[SEPARATOR_POSITION_+1:]}</b>",
                             parse_mode="HTML",
                         )
                     if "/w3w" in text:
-                        location = get_location(
-                            session, headers, account_id, vin)
+                        location = get_location(session, headers, account_id, vin)
                         lon = location["data"]["attributes"]["gpsLongitude"]
                         lat = location["data"]["attributes"]["gpsLatitude"]
                         w3w_url = f"https://api.what3words.com/v3/convert-to-3wa?coordinates={lat},{lon}"
-                        with requests.get(w3w_url, headers={
-                            "x-api-key": os.getenv("W3W_KEY"),
-                            "format": "json"
-                            }) as w3w_req:
+                        with requests.get(
+                            w3w_url,
+                            headers={
+                                "x-api-key": os.getenv("W3W_KEY"),
+                                "format": "json",
+                            },
+                        ) as w3w_req:
                             if w3w_req.status_code == 200:
                                 w3w_data = w3w_req.json()
-                                print(w3w_data)
                                 w3w_data = w3w_data["words"].split(".")
                                 send_message(
                                     f"*[{w3w_data[0]}\\.{w3w_data[1]}\\.{w3w_data[2]}](https://w3w.co/{w3w_data[0]}\\.{w3w_data[1]}\\.{w3w_data[2]})*",
@@ -271,7 +276,7 @@ def run():
         time.sleep(1)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     while True:
         try:
             run()
